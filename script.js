a5c7308c8f7c8298781c1ce7f0623ee72cd47c03
shazomii <seyeoyeniran2u@gmail.com>
2025-06-30 17:24:21 +0100

    Add basic logic to create and save a new book

---
+++ script.js
@@ -0,0 +1,14 @@
+const myLibrary = [];
+
+function Book(id, title, author, pages, read) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    let newBook = new Book(crypto.randomUUID(), title, author, pages, read);
+    myLibrary.push(newBook);
+}
