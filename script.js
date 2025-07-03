b0a62141e982de0eb11728654274aa4f567d646e
shazomii <seyeoyeniran2u@gmail.com>
2025-07-03 13:34:03 +0100

    Enhance book rendering and interaction logic in the library

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const myLibrary = [];
 
 const container = document.querySelector(".books-container");
 const newBookButton = document.querySelector(".button-new-book");
-const submitButton = document.querySelector(".button-submit");
 const cancelButton = document.querySelector(".button-cancel");
 const addBookModal = document.querySelector(".add-book-modal");
 const form = document.querySelector(".book-form");
@@ -25,6 +24,60 @@ function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(newBook);
     return newBook;
 }
+
+addBookToLibrary("Bury your Bones on the Midnight Soil", "V. E Schwab", 400, false);
+addBookToLibrary("Sounds Like Love", "Ashley Preston", 650, true);
+addBookToLibrary("King of Ashes", "S. A. Cosby", 354, true);
+addBookToLibrary("The River is Waiting", "Wally Lamb", 989, false);
+addBookToLibrary("The Poppy Fields", "Nikki Erlick", 675, true);
+addBookToLibrary("We don't talk about Carol", "Kristen L. Berry", 437, false);
+addBookToLibrary("Welcome to murder week", "Karen Dukess", 983, true);
+addBookToLibrary("What kind of paradise", "Janelle Brown", 748, true);
+
+function preRenderBook() {
+    myLibrary.forEach(book => {
+        const bookItem = document.createElement("div");
+        const bookDetails = document.createElement("div");
+        const bookActions = document.createElement("div");
+        const bookTitle = document.createElement("p");
+        const bookAuthor = document.createElement("p");
+        const bookPages = document.createElement("p");
+        const btnRead = document.createElement("button");
+        const btnDel = document.createElement("button");
+
+        bookItem.className = "book";
+        bookDetails.className = "book-details";
+        bookActions.className = "book-actions";
+        bookTitle.className = "book-title";
+        bookAuthor.className = "book-author";
+        bookPages.className = "book-pages";
+        btnRead.className = "button-read";
+        btnDel.className = "button-delete";
+
+        bookItem.setAttribute("data-id", book.id);
+        bookTitle.textContent = book.title;
+        bookAuthor.textContent = book.author;
+        bookPages.textContent = `${book.pages} pages`;
+        btnRead.textContent = book.read ? "Mark unread" : "Mark as read";
+        btnDel.textContent = "Remove";
+
+        bookDetails.append(bookTitle, bookAuthor, bookPages);
+        bookActions.append(btnRead, btnDel);
+        bookItem.append(bookDetails, bookActions);
+        container.appendChild(bookItem);
+
+        btnRead.addEventListener('click', () => {
+            book.toggleRead();
+            btnRead.textContent = book.read ? "Mark unread" : "Mark as read";
+        })
+
+        btnDel.addEventListener('click', () => {
+            deleteBook(book)
+        });
+    })
+}
+
+preRenderBook();
 
 function renderBook(book) {
 
@@ -50,10 +103,8 @@ function renderBook(book) {
     bookTitle.textContent = book.title;
     bookAuthor.textContent = book.author;
     bookPages.textContent = `${book.pages} pages`;
-    btnRead.textContent = book.read ? "Mark as unread" : "Mark as read";
+    btnRead.textContent = book.read ? "Mark unread" : "Mark as read";
     btnDel.textContent = "Remove";
-
-    btnRead.style.backgroundColor = book.read ? "green" : "orange";
 
     bookDetails.append(bookTitle, bookAuthor, bookPages);
     bookActions.append(btnRead, btnDel);
@@ -62,8 +113,7 @@ function renderBook(book) {
 
     btnRead.addEventListener('click', () => {
         book.toggleRead();
-        btnRead.textContent = book.read ? "Mark as unread" : "Mark as read";
-        btnRead.style.backgroundColor = book.read ? "green" : "orange";
+        btnRead.textContent = book.read ? "Mark unread" : "Mark as read";
     })
 
     btnDel.addEventListener('click', () => {
@@ -81,17 +131,13 @@ function deleteBook(book) {
     }
 }
 
-function closeResetModal() {
-    form.reset();
-    addBookModal.close();
-}
-
 newBookButton.addEventListener('click', () => {
     addBookModal.showModal();
 });
 
-submitButton.addEventListener('click', (e) => {
+form.addEventListener('submit', (e) => {
     e.preventDefault();
+
     const formData = {};
 
     form.querySelectorAll("input").forEach(element => {
@@ -100,15 +146,16 @@ submitButton.addEventListener('click', (e) => {
         } else {
             formData[element.name] = element.value;
         }
-    });    
+    });
 
     const addedBook = addBookToLibrary(formData.title, formData.author, formData.pages, formData.read);
 
     renderBook(addedBook);
 
-    closeResetModal();
+    form.reset();
+    addBookModal.close();
 });
 
 cancelButton.addEventListener('click', () => {
-    closeResetModal();
+    addBookModal.close();
 });
